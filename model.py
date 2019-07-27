e4e32bff7acb2dfb3a2025bdd27a94a175093f39
Arun Jose <arun.jose@gmail.com>
2019-07-27 20:44:26 +0000

    Add left and right camera images. The model works with this setting!!!!

--- model.py
+++ model.py
@@ -22,17 +22,32 @@ with open('data/driving_log.csv') as csvfile:
 
 images = []
 measurements = []
+steering_correction = 0.1
 for line in lines:
     source_path = line[0]
+    source_path_left = line[1]
+    source_path_right = line[2]
     if line[0] == 'center':
         continue
     filename = source_path.split('/')[-1]
+    filename_left = source_path_left.split('/')[-1]
+    filename_right = source_path_right.split('/')[-1]
     current_path = 'data/IMG/' + filename
+    current_path_left = 'data/IMG/' + filename_left
+    current_path_right = 'data/IMG/' + filename_right
     #image = cv2.imread(current_path)
     image = ndimage.imread(current_path)
+    image_left = ndimage.imread(current_path_left)
+    image_right = ndimage.imread(current_path_right)
     images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
+    images.append(image_left)
+    images.append(image_right)
+    steering_center = float(line[3])
+    steering_left = steering_center + steering_correction
+    steering_right = steering_center - steering_correction
+    measurements.append(steering_center)
+    measurements.append(steering_left)
+    measurements.append(steering_right)
 
 #NOTE: cv2.imread will get images in BGR format, while drive.py uses RGB. In
 #the video above one way you could keep the same image formatting is to do
@@ -76,21 +91,21 @@ def expand_dataset(X_input, y_input, threshold=0.25):
 
     return X_duplicate, y_duplicate
 
-X_duplicate, y_duplicate = expand_dataset(X_train, y_train, threshold=0.2)
-print('Before nsamples=', len(y_train))
-X_train = np.concatenate((X_train, X_duplicate), axis=0)
-y_train = np.concatenate((y_train, y_duplicate), axis=0)
-print('After nsamples=', len(y_train))
+#X_duplicate, y_duplicate = expand_dataset(X_train, y_train, threshold=0.2)
+#print('Before nsamples=', len(y_train))
+#X_train = np.concatenate((X_train, X_duplicate), axis=0)
+#y_train = np.concatenate((y_train, y_duplicate), axis=0)
+#print('After nsamples=', len(y_train))
 
-#fignum = 0
-#fignum += 1
-#plt.figure(fignum)
-#plt.hist(y_train, bins=20)  # arguments are passed to np.histogram
-#plt.title("Histogram for training data")
-#plt.xlabel("Class label")
-#plt.ylabel("Number of samples")
-#plt.savefig("histogram_training.png")
-#sys.exit(1)
+# fignum = 0
+# fignum += 1
+# plt.figure(fignum)
+# plt.hist(y_train, bins=20)  # arguments are passed to np.histogram
+# plt.title("Histogram for training data")
+# plt.xlabel("Class label")
+# plt.ylabel("Number of samples")
+# plt.savefig("histogram_training.png")
+# sys.exit(1)
 
 model = Sequential()
 model.add(Lambda(lambda x: (x - 128.0)/128.0, input_shape = (160, 320, 3)))
@@ -130,4 +145,3 @@ model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=2)
 # plt.show()
 
 model.save('model.h5')
-
