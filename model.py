25b2abe6aaddeb4de00fda5c53343901a1c7e772
aijose <arun.jose@gmail.com>
2019-07-27 12:54:59 +0000

    Add initial versin of model.py

---
+++ model.py
@@ -0,0 +1,72 @@
+import csv
+import cv2
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from scipy import ndimage
+
+lines = []
+with open('data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    if line[0] == 'center':
+        continue
+    filename = source_path.split('/')[-1]
+    current_path = 'data/IMG/' + filename
+    #image = cv2.imread(current_path)
+    image = ndimage.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+#NOTE: cv2.imread will get images in BGR format, while drive.py uses RGB. In
+#the video above one way you could keep the same image formatting is to do
+#"image = ndimage.imread(current_path)" with "from scipy import ndimage"
+#instead.
+
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(-1.0*measurement)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160, 320, 3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Flatten())
+#model.add(Flatten(input_shape = (160, 320, 3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+
+#history_object = model.fit_generator(train_generator, samples_per_epoch =
+#    len(train_samples), validation_data = 
+#    validation_generator,
+#    nb_val_samples = len(validation_samples), 
+#    nb_epoch=5, verbose=1)
+
+### print the keys contained in the history object
+#print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+# plt.plot(history_object.history['loss'])
+# plt.plot(history_object.history['val_loss'])
+# plt.title('model mean squared error loss')
+# plt.ylabel('mean squared error loss')
+# plt.xlabel('epoch')
+# plt.legend(['training set', 'validation set'], loc='upper right')
+# plt.show()
+
+model.save('model.h5')
