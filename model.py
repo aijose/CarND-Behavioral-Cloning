ec9f7ddfe3bc36812b45ca24ee11ce7953d2ff26
aijose <arun.jose@gmail.com>
2019-07-30 17:46:51 -0600

    Delete unwanted code

--- model.py
+++ model.py
@@ -137,22 +137,3 @@ if flag_model_summary:
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=2)
 model.save('model.h5')
-
-#history_object = model.fit_generator(train_generator, samples_per_epoch =
-#    len(train_samples), validation_data = 
-#    validation_generator,
-#    nb_val_samples = len(validation_samples), 
-#    nb_epoch=5, verbose=1)
-
-### print the keys contained in the history object
-#print(history_object.history.keys())
-
-### plot the training and validation loss for each epoch
-# plt.plot(history_object.history['loss'])
-# plt.plot(history_object.history['val_loss'])
-# plt.title('model mean squared error loss')
-# plt.ylabel('mean squared error loss')
-# plt.xlabel('epoch')
-# plt.legend(['training set', 'validation set'], loc='upper right')
-# plt.show()
-
