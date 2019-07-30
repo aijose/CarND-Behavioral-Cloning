eafcb0e272e718bff061cc3a0c53d6c26154a80d
aijose <arun.jose@gmail.com>
2019-07-30 16:22:05 -0600

    Cleanup data and add flags for controling workflow

--- model.py
+++ model.py
@@ -1,19 +1,26 @@
 import csv
 import cv2
 import numpy as np
-#import sys
+import sys
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D, Dense
 from scipy import ndimage
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-seed_value = 123
 from numpy.random import seed
-seed(seed_value)
 from tensorflow import set_random_seed
+seed_value = 123
+seed(seed_value)
 set_random_seed(seed_value)
 
+# Define flags for controlling workflow
+flag_use_side_cameras = True
+flag_add_noisy_data = False
+flag_plot_histogram = False
+flag_model_summary =  False
+
+# Read data in the driving log
 lines = []
 with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -25,35 +32,33 @@ measurements = []
 steering_correction = 0.1
 for line in lines:
     source_path = line[0]
-    source_path_left = line[1]
-    source_path_right = line[2]
     if line[0] == 'center':
         continue
     filename = source_path.split('/')[-1]
-    filename_left = source_path_left.split('/')[-1]
-    filename_right = source_path_right.split('/')[-1]
     current_path = 'data/IMG/' + filename
-    current_path_left = 'data/IMG/' + filename_left
-    current_path_right = 'data/IMG/' + filename_right
-    #image = cv2.imread(current_path)
     image = ndimage.imread(current_path)
-    image_left = ndimage.imread(current_path_left)
-    image_right = ndimage.imread(current_path_right)
     images.append(image)
-    images.append(image_left)
-    images.append(image_right)
     steering_center = float(line[3])
-    steering_left = steering_center + steering_correction
-    steering_right = steering_center - steering_correction
     measurements.append(steering_center)
-    measurements.append(steering_left)
-    measurements.append(steering_right)
 
-#NOTE: cv2.imread will get images in BGR format, while drive.py uses RGB. In
-#the video above one way you could keep the same image formatting is to do
-#"image = ndimage.imread(current_path)" with "from scipy import ndimage"
-#instead.
+    # Augment center camera data with side cameras
+    if flag_use_side_cameras:
+        source_path_left = line[1]
+        source_path_right = line[2]
+        filename_left = source_path_left.split('/')[-1]
+        filename_right = source_path_right.split('/')[-1]
+        current_path_left = 'data/IMG/' + filename_left
+        current_path_right = 'data/IMG/' + filename_right
+        image_left = ndimage.imread(current_path_left)
+        image_right = ndimage.imread(current_path_right)
+        images.append(image_left)
+        images.append(image_right)
+        steering_left = steering_center + steering_correction
+        steering_right = steering_center - steering_correction
+        measurements.append(steering_left)
+        measurements.append(steering_right)
 
+# Augment data with flipped images
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
     augmented_images.append(image)
@@ -91,27 +96,31 @@ def expand_dataset(X_input, y_input, threshold=0.25):
 
     return X_duplicate, y_duplicate
 
-#X_duplicate, y_duplicate = expand_dataset(X_train, y_train, threshold=0.2)
-#print('Before nsamples=', len(y_train))
-#X_train = np.concatenate((X_train, X_duplicate), axis=0)
-#y_train = np.concatenate((y_train, y_duplicate), axis=0)
-#print('After nsamples=', len(y_train))
+if flag_add_noisy_data:
+    X_duplicate, y_duplicate = expand_dataset(X_train, y_train, threshold=0.2)
+    print('Number of samples before adding noise =', len(y_train))
+    X_train = np.concatenate((X_train, X_duplicate), axis=0)
+    y_train = np.concatenate((y_train, y_duplicate), axis=0)
+    print('Number of samples after adding noise =', len(y_train))
 
-# fignum = 0
-# fignum += 1
-# plt.figure(fignum)
-# plt.hist(y_train, bins=20)  # arguments are passed to np.histogram
-# plt.title("Histogram for training data")
-# plt.xlabel("Class label")
-# plt.ylabel("Number of samples")
-# plt.savefig("histogram_training.png")
-# sys.exit(1)
+if flag_plot_histogram:
+    #Plot histogram of data
+    fignum = 0
+    fignum += 1
+    plt.figure(fignum)
+    plt.hist(y_train, bins=20)  # arguments are passed to np.histogram
+    plt.title("Histogram for training data")
+    plt.xlabel("Steering angle")
+    plt.ylabel("Number of samples")
+    plt.savefig("histogram.png")
+
+# Exit if you don't want to go any further
+#sys.exit(1)
 
 model = Sequential()
 model.add(Lambda(lambda x: (x - 128.0)/128.0, input_shape = (160, 320, 3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
 model.add(Conv2D(24, (5, 5), activation='relu', padding='valid'))
-#model.add(Conv2D(24, (5, 5), activation='relu', padding='valid'))
 model.add(Conv2D(36, (5, 5), activation='relu', padding='valid'))
 model.add(Conv2D(48, (5, 5), activation='relu', padding='valid'))
 #model.add(Conv2D(64, (3, 3), activation='relu', padding='valid'))
@@ -121,10 +130,13 @@ model.add(Dense(100, activation='relu'))
 model.add(Dense(50, activation='relu'))
 model.add(Dense(10, activation='relu'))
 model.add(Dense(1))
-#model.summary()
+
+if flag_model_summary:
+    model.summary()
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=2)
+model.save('model.h5')
 
 #history_object = model.fit_generator(train_generator, samples_per_epoch =
 #    len(train_samples), validation_data = 
@@ -144,4 +156,3 @@ model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=2)
 # plt.legend(['training set', 'validation set'], loc='upper right')
 # plt.show()
 
-model.save('model.h5')
